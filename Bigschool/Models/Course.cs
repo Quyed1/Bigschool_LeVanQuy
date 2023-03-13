c8a71b960b440084db6160f8a237690eec25e473
Admin <lequy332@gmail.com>
2023-03-13 17:47:30 +0700

    Add project files.

---
+++ Bigschool/Models/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bigschool.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public ApplicationUser Lencturer { get; set; }
+        [Required]
+        public string LencturerId { get; set; }
+        [StringLength(255)]
+        public string Place { get; set; }
+        public DateTime DateTime { get; set; }
+        public Category Category { get; set; }
+        [Required]
+        public byte CategoryId { get; set; }
+    }
+}
